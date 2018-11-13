edbb689be9f025a4165e7a4baf96125c4cd0257a
Maxim Zimaliev <zimaliev@gmail.com>
2018-11-13 22:41:20 +0300

    update for ps0.12

--- src/Clipboard.purs
+++ src/Clipboard.purs
@@ -8,41 +8,35 @@ module Clipboard
 
 import Prelude
 
-import Control.Monad.Eff (Eff)
-import DOM (DOM)
+import Effect (Effect)
 import CSS (Selector, selector)
-import DOM.Node.Types (Element)
+import Web.DOM.Element (Element)
 
 foreign import data Clipboard :: Type
 
 foreign import fromElement
-  :: forall eff
-   . Element
-  -> Eff (dom :: DOM | eff) String
-  -> Eff (dom :: DOM | eff) Clipboard
+  :: Element
+  -> Effect String
+  -> Effect Clipboard
 
 fromCSSSelector
-  :: forall eff
-   . Selector
-  -> (Element -> Eff (dom :: DOM | eff) String)
-  -> Eff (dom :: DOM | eff) Clipboard
+  :: Selector
+  -> (Element -> Effect String)
+  -> Effect Clipboard
 fromCSSSelector = selector >>> fromStringSelector
 
 foreign import fromStringSelector
-  :: forall eff
-   . String
-  -> (Element -> Eff (dom :: DOM | eff) String)
-  -> Eff (dom :: DOM | eff) Clipboard
+  :: String
+  -> (Element -> Effect String)
+  -> Effect Clipboard
 
 -- | Registers a click handler on an Event, which triggers the passed `Eff` and
 -- | copies the text inside the returned element to the clipboard.
 foreign import fromElementWithTarget
-  :: forall eff
-   . Element
-  -> Eff (dom :: DOM | eff) Element
-  -> Eff (dom :: DOM | eff) Clipboard
+  :: Element
+  -> Effect Element
+  -> Effect Clipboard
 
 foreign import destroy
-  :: forall eff
-   . Clipboard
-  -> Eff (dom :: DOM | eff) Unit
+  :: Clipboard
+  -> Effect Unit
